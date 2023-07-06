660f27af27b6bab22bc78ebcf91a46fc880664b6
line <vonlinee@163.com>
2023-07-06 23:29:15 +0800

    MonacoEditor组件使用render函数创建

--- devpl-webui/src/components/editor/MonacoEditor.vue
+++ devpl-webui/src/components/editor/MonacoEditor.vue
@@ -1,100 +1,124 @@
-<script setup lang="ts">
+<script lang="ts">
 import * as monaco from 'monaco-editor'
 import {editor} from 'monaco-editor'
-import {onMounted, reactive, ref} from 'vue'
+import {defineComponent, h, onMounted, reactive, ref, toRefs} from 'vue'
 import IStandaloneEditorConstructionOptions = editor.IStandaloneEditorConstructionOptions;
 import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
 import ITextModel = editor.ITextModel;
 
-// 编辑器实例
-let monacoEditor: monaco.editor.IStandaloneCodeEditor | undefined = undefined
+// 可用的语言选项
+const languageOptions = ref([
+    "bat", "cpp", "css", "dockerfile", "go", "graphql", "html", "ini",
+    "java", "javascript", "json", "julia", "kotlin", "less", "markdown", "mysql", "objective-c", "pascal", "pascaligo",
+    "perl", "php", "powershell", "python", "r", "redis", "rust", "scala", "scheme", "scss", "shell",
+    "sophia", "sql", "swift", "tcl", "typescript", "xml", "yaml"])
 
-/**
- * 组件暴露的属性
- */
-interface EditorProps {
-    // 初始值
-    value?: string
-    // 初始语言
-    language: 'javascript' | 'typescript' | 'css' | 'json' | 'xml' | 'java' | 'freemarker' | 'velocity' | 'sql'
-    // 编辑器选项，如果为null则使用默认选项
-    editorOptions?: object
-}
-
-const props = defineProps<EditorProps>()
-
-let editorBoxRef = ref()
-
-let content = ref()
-
-const editorOptions: IStandaloneEditorConstructionOptions = reactive({
-    value: props.value || '', // 编辑器初始显示文字
-    language: props.language, // 语言支持
-    minimap: {enabled: false},
-    fontSize: 15,
-    automaticLayout: true, // 自适应布局
-    theme: 'vs',
-    foldingStrategy: 'indentation',
-    renderLineHighlight: 'all', // 行亮
-    selectOnLineNumbers: true, // 显示行号
-    readOnly: false, // 只读
-    roundedSelection: false,
-    scrollBeyondLastLine: false, // 取消代码后面一大段空白
-    overviewRulerBorder: false, // 不要滚动条的边框
-    ...props.editorOptions
-})
-
-onMounted(() => {
-    if (!monacoEditor) {
-        monacoEditor = createMonacoEditor(editorBoxRef.value)
-    }
-})
-
-/**
- * 创建编辑器节点
- * @param domElement
- */
-function createMonacoEditor(domElement: HTMLElement): IStandaloneCodeEditor {
-    return monaco.editor.create(domElement!, editorOptions)
-}
-
-/**
- * 暴露API
- */
-defineExpose({
-    getText: function () {
-        return monacoEditor?.getValue()
-    },
-    setText: function (text: string) {
-        monacoEditor?.setValue(text)
-    },
-    /**
-     * 设置语言模式 https://github.com/Microsoft/monaco-editor/issues/539
-     * @param lang
-     */
-    setLanguage(lang: string) {
-        if (monacoEditor) {
-            const textModel: ITextModel | null = monacoEditor.getModel()
-            if (textModel) {
-                monaco.editor.setModelLanguage(textModel, lang)
-            }
+export default defineComponent({
+    name: 'MonacoEditor',
+    props: {
+        // 初始宽度
+        width: {
+            type: String,
+            default: '100%',
+            required: false
+        },
+        // 初始高度
+        height: {
+            type: String,
+            default: '100%',
+            required: false
+        },
+        // 初始值
+        text: {
+            type: String,
+            required: false,
+        },
+        // 初始语言
+        language: {
+            type: String,
+            required: true,
+        },
+        options: {
+            type: Object,
+            required: false
         }
+    },
+    setup(props: any, context) {
+        const {language, text, width, height} = toRefs(props)
+        const editorOptions: IStandaloneEditorConstructionOptions = reactive({
+            value: text.value, // 编辑器初始显示文字
+            language: language.value, // 语言支持
+            minimap: {enabled: false},
+            fontSize: 15,
+            automaticLayout: true, // 自适应布局
+            theme: 'vs',
+            foldingStrategy: 'indentation',
+            renderLineHighlight: 'all', // 行亮
+            selectOnLineNumbers: true, // 显示行号
+            readOnly: false, // 只读
+            roundedSelection: false,
+            scrollBeyondLastLine: false, // 取消代码后面一大段空白
+            overviewRulerBorder: false, // 不要滚动条的边框
+        })
+
+        /**
+         * 创建编辑器节点
+         * @param domElement
+         */
+        function createMonacoEditor(domElement: HTMLElement): IStandaloneCodeEditor {
+            return monaco.editor.create(domElement!, editorOptions)
+        }
+
+        // 编辑器容器引用
+        const editorBoxRef = ref()
+
+        // 编辑器实例
+        let monacoEditor: monaco.editor.IStandaloneCodeEditor | undefined = undefined
+        onMounted(() => {
+            if (!monacoEditor) {
+                monacoEditor = createMonacoEditor(editorBoxRef.value)
+            }
+        })
+
+        /**
+         * 暴露API
+         */
+        context.expose({
+            getText: function () {
+                return monacoEditor?.getValue()
+            },
+            setText: function (text: string) {
+                monacoEditor?.setValue(text)
+            },
+            /**
+             * 设置语言模式 https://github.com/Microsoft/monaco-editor/issues/539
+             * @param lang
+             */
+            setLanguage(lang: string) {
+                if (monacoEditor) {
+                    const textModel: ITextModel | null = monacoEditor.getModel()
+                    if (textModel) {
+                        editorOptions.language = lang
+                        monaco.editor.setModelLanguage(textModel, lang)
+                    }
+                }
+            },
+            getLanguage() {
+                return editorOptions.language;
+            }
+        })
+        return () => h("div", {
+            ref: editorBoxRef,
+            class: 'editor-container',
+            style: {
+                width: width.value,
+                height: height.value,
+                margin: 0,
+                padding: 0,
+                textAlign: 'left',
+                display: 'inline-block'
+            }
+        }, [])
     }
-})
-
+});
 </script>
-
-<template>
-    <div id="editor-box" ref="editorBoxRef"></div>
-</template>
-
-<style scoped lang="scss">
-#editor-box {
-    height: 100%;
-    width: 100%;
-    margin: 0;
-    padding: 0;
-    text-align: left;
-    display: inline-block;
-}
-</style>
--- devpl-webui/src/views/test/index.vue
+++ devpl-webui/src/views/test/index.vue
@@ -2,17 +2,40 @@
 
 import {ref} from "vue";
 import AdaptiveDialog from "@/components/AdaptiveDialog.vue";
+import MonacoEditor from "@/components/editor/MonacoEditor.vue";
 
 let dialogRef = ref()
 
 function show() {
     dialogRef.value.init()
 }
+
+const editorRef = ref()
+const languageRef = ref()
+
+const inputRef = ref()
+
+function updateLanguage() {
+    languageRef.value = editorRef.value.getLanguage()
+}
+
+function setLanguage() {
+    editorRef.value.setLanguage(inputRef.value)
+}
+
 </script>
 
 <template>
-    <button @click="show">Show</button>
+    <button @click="show">展示弹窗</button>
+    <button @click="updateLanguage()">更新语言</button>
+    <button @click="setLanguage()">设置语言</button>
+
+    <el-input v-model="inputRef"></el-input>
     <adaptive-dialog ref="dialogRef"></adaptive-dialog>
+
+    <monaco-editor ref="editorRef" text="helloworld" language="typescript" width="500px" height="300px"></monaco-editor>
+
+    {{ languageRef }}
 </template>
 
 <style scoped lang="scss">
