e0780eda616c7c4fc4918d4ec14b1a676280701a
line <vonlinee@163.com>
2023-10-08 23:17:11 +0800

    1

---
+++ devpl-backend/devpl-fxui/src/main/java/io/devpl/fxui/view/MainView.java
@@ -0,0 +1,12 @@
+package io.devpl.fxui.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.show();
+    }
+}
