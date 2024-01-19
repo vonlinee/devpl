4dceb060a434fe22bef16c583ddf0198200e407d
vonlinee <vonlinee@163.com>
2024-01-19 15:30:43 +0800

    移植可拖拽的表格

---
+++ devpl-webui/src/components/treetable/Space.vue
@@ -0,0 +1,28 @@
+<template>
+    <span class="space-container">
+        <span class="space" v-for="(item, index) in spaces" v-bind:key="index"></span>
+    </span>
+</template>
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const { depth } = defineProps<{
+    depth: number
+}>()
+
+const spaces = computed(() => {
+    const aArr = []
+    for (let i = 0; i < depth; i++) {
+        aArr.push('')
+    }
+    return aArr
+})
+
+</script>
+<style>
+.space {
+    display: inline-block;
+    width: 15px;
+}
+</style>
+  
--- devpl-webui/src/views/test/index.vue
+++ devpl-webui/src/views/test/index.vue
@@ -1,11 +1,17 @@
 <template>
+  
   <Child :count="count"></Child>
+
+  <HelloWorld></HelloWorld>
+
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from "vue"
 import Child from "./Child.vue"
+
 import { apiListTemplateTypes } from "@/api/template"
+import HelloWorld from "@/components/treetable/HelloWorld.vue";
 
 const count = ref()
 
