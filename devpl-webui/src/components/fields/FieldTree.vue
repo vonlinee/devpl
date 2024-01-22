60176d1ee5af3d2fa548a34c513fb51f211e8d4e
line <vonlinee@163.com>
2024-01-22 23:54:49 +0800

    1

--- devpl-webui/src/components/fields/FieldTree.vue
+++ devpl-webui/src/components/fields/FieldTree.vue
@@ -21,34 +21,35 @@
       @node-drop="handleDrop"
     >
       <template #default="{ node, data }">
-        <div class="field-tree-node-container">
-          <!-- 字段名称 -->
-          <a
-            v-if="!(data.editing || false)"
-            class="field-label"
-            :title="data.description || '无'"
-            @click="fireInput(data)"
+        <div style="height: auto">
+          <div class="field-tree-node-container">
+            <!-- 字段名称 -->
+            <a
+              v-if="!(data.editing || false)"
+              class="field-label"
+              :title="data.description || '无'"
+              @click="fireInput(data)"
             >{{ data.fieldKey }}</a
-          >
-          <input
-            v-if="data.editing || false"
-            ref="currentInputRef"
-            class="field-input"
-            @blur="data.editing = false"
-            @change="(e) => onInputChange(e, data)"
-            @keyup.enter="(e) => onInputChange(e, data)"
-          />
+            >
+            <input
+              v-if="data.editing || false"
+              ref="currentInputRef"
+              class="field-input"
+              @blur="data.editing = false"
+              @change="(e) => onInputChange(e, data)"
+              @keyup.enter="(e) => onInputChange(e, data)"
+            />
 
-          <!-- 数据类型-->
-          <el-button
-            v-if="!(data.editing || false)"
-            link
-            style="margin-left: 10px; color: green"
-          >
-            {{ data.dataType || "unknown" }}
-          </el-button>
+            <!-- 数据类型-->
+            <el-button
+              v-if="!(data.editing || false)"
+              link
+              style="margin-left: 10px; color: green"
+            >
+              {{ data.dataType || "unknown" }}
+            </el-button>
 
-          <div class="operation-btn-container">
+            <div class="operation-btn-container">
             <span style="display: inline-block; height: 100%">
               <el-button link :icon="Plus" @click="append(data)"></el-button>
               <el-button
@@ -58,7 +59,9 @@
                 @click="remove(node, data)"
               ></el-button>
             </span>
+            </div>
           </div>
+          <div class="description" :draggable="false">{{ data.description }}</div>
         </div>
       </template>
     </el-tree>
@@ -66,30 +69,30 @@
 </template>
 
 <script lang="ts" setup>
-import { StyleValue, nextTick, reactive, ref } from "vue"
-import type Node from "element-plus/es/components/tree/src/model/node"
-import type { DragEvents } from "element-plus/es/components/tree/src/model/useDragNode"
+import { StyleValue, nextTick, reactive, ref } from "vue";
+import type Node from "element-plus/es/components/tree/src/model/node";
+import type { DragEvents } from "element-plus/es/components/tree/src/model/useDragNode";
 import type {
   AllowDropType,
-  NodeDropType,
-} from "element-plus/es/components/tree/src/tree.type"
-import { Minus, Plus } from "@element-plus/icons"
-import { watch } from "vue"
+  NodeDropType
+} from "element-plus/es/components/tree/src/tree.type";
+import { Minus, Plus } from "@element-plus/icons";
+import { watch } from "vue";
 
-const currentInputRef = ref()
+const currentInputRef = ref();
 
 const fireInput = (data: FieldInfo) => {
-  data.editing = true
+  data.editing = true;
   nextTick(() => {
-    currentInputRef.value.value = data.fieldKey
-    currentInputRef.value.focus()
-  })
-}
+    currentInputRef.value.value = data.fieldKey;
+    currentInputRef.value.focus();
+  });
+};
 
 const onInputChange = (event: Event, data: FieldInfo) => {
-  data.fieldKey = (event.target as HTMLInputElement).value
-  data.editing = false
-}
+  data.fieldKey = (event.target as HTMLInputElement).value;
+  data.editing = false;
+};
 
 /**
  * 组件属性
@@ -105,46 +108,47 @@ const { selectable, height, fields } = withDefaults(
   defineProps<FieldTreeProps>(),
   {
     selectable: false,
-    height: "100%",
+    height: "100%"
   }
-)
+);
 
-const dataSource = ref<FieldInfo[]>(fields || [])
+const dataSource = ref<FieldInfo[]>(fields || []);
 
 const addIfEmpty = () => {
   if (dataSource.value.length == 0) {
     dataSource.value.push({
       id: 0,
       editing: true,
-      fieldKey: "",
-    })
+      fieldKey: ""
+    });
   }
-}
+};
 
 /**
  * 容器样式
  */
 const containerStyle = reactive({
   height: height,
-  overflowY: "scroll",
-}) as StyleValue
+  overflowY: "scroll"
+}) as StyleValue;
 
-let id = 1000
+let id = 1000;
 
 /**
  * 追加节点
  * @param data 节点数据
  */
 const append = (data: FieldInfo) => {
-  const newChild = { id: id++, fieldKey: "New Item", children: [] } as FieldInfo
+  const newChild = { id: id++, fieldKey: "New Item", children: [] } as FieldInfo;
   if (!data.children) {
-    data.children = []
+    data.children = [];
   }
-  data.children.push(newChild)
-  dataSource.value = [...dataSource.value]
-}
+  data.children.push(newChild);
+  dataSource.value = [...dataSource.value];
+};
 
-const handleNodeClicked = (data: any, node: any, item: any) => {}
+const handleNodeClicked = (data: any, node: any, item: any) => {
+};
 
 /**
  * 删除节点
@@ -153,14 +157,14 @@ const handleNodeClicked = (data: any, node: any, item: any) => {}
  */
 const remove = (node: Node, data: FieldInfo) => {
   if (node.label === "Root") {
-    return
+    return;
   }
-  const parent = node.parent
-  const children: FieldInfo[] = parent.data.children || parent.data
-  const index = children.findIndex((d) => d.id === data.id)
-  children.splice(index, 1)
-  dataSource.value = [...dataSource.value]
-}
+  const parent = node.parent;
+  const children: FieldInfo[] = parent.data.children || parent.data;
+  const index = children.findIndex((d) => d.id === data.id);
+  children.splice(index, 1);
+  dataSource.value = [...dataSource.value];
+};
 
 /**
  * 是否允许放下拖拽的节点
@@ -170,70 +174,93 @@ const remove = (node: Node, data: FieldInfo) => {
  */
 const allowDrop = (draggingNode: Node, dropNode: Node, type: AllowDropType) => {
   if (dropNode.data.fieldKey === "Level two 3-1") {
-    return type !== "inner"
+    return type !== "inner";
   } else {
-    return true
+    return true;
   }
-}
+};
 
 /**
  * 是否允许开始拖拽
  * @param draggingNode
  */
 const allowDrag = (draggingNode: Node) => {
-  return draggingNode.data.editing !== true
-}
+  return draggingNode.data.editing !== true;
+};
 
 const handleDragStart = (node: Node, ev: DragEvents) => {
-  console.log("drag start", node)
-}
+  console.log("drag start", node);
+};
 
 const handleDragEnter = (
   draggingNode: Node,
   dropNode: Node,
   ev: DragEvents
-) => {}
+) => {
+};
 const handleDragLeave = (
   draggingNode: Node,
   dropNode: Node,
   ev: DragEvents
-) => {}
+) => {
+};
 const handleDragOver = (
   draggingNode: Node,
   dropNode: Node,
   ev: DragEvents
-) => {}
+) => {
+};
 const handleDragEnd = (
   draggingNode: Node,
   dropNode: Node,
   dropType: NodeDropType,
   ev: DragEvents
-) => {}
+) => {
+};
 const handleDrop = (
   draggingNode: Node,
   dropNode: Node,
   dropType: NodeDropType,
   ev: DragEvents
-) => {}
+) => {
+};
 
 watch(
   () => fields,
-  (newValue, oldValue) => {},
+  (newValue, oldValue) => {
+  },
   {
-    deep: true,
+    deep: true
   }
-)
+);
 
 defineExpose({
-  getFields: function () {
-    return dataSource.value
-  },
-})
+  getFields: function() {
+    return dataSource.value;
+  }
+});
 </script>
 
 <style lang="scss" scoped>
+
+// 每个节点高度由其节点的子节点决定
+::v-deep(.el-tree-node__content) {
+  height: auto;
+}
+
+.description {
+  margin-left: 11px;
+  font-size: 12px;
+  /*这两行代码可以解决大部分场景下的换行问题*/
+  word-break: break-all;
+  word-wrap: break-word;
+  /*但在有些场景中，还需要加上下面这行代码*/
+  white-space: normal;
+}
+
 .field-tree-node-container {
   width: 100%;
+  height: 30px;
   display: flex;
   align-items: center;
   //justify-content: space-between;
@@ -275,9 +302,9 @@ defineExpose({
     border-color: #66afe9;
     outline: 0;
     -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
-      0 0 8px rgba(102, 175, 233, 0.6);
+    0 0 8px rgba(102, 175, 233, 0.6);
     box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
-      0 0 8px rgba(102, 175, 233, 0.6);
+    0 0 8px rgba(102, 175, 233, 0.6);
   }
 }
 </style>
--- devpl-webui/src/views/test/index.vue
+++ devpl-webui/src/views/test/index.vue
@@ -1,25 +1,36 @@
 <template>
 
-<!--  <Child :count="count"></Child>-->
+  <!--  <Child :count="count"></Child>-->
 
-  <FieldTreeTableV1></FieldTreeTableV1>
-<!--  <HelloWorld></HelloWorld>-->
+  <!--  <FieldTreeTableV1></FieldTreeTableV1>-->
+  <!--  <HelloWorld></HelloWorld>-->
+
+  <FieldTree :fields="fields"></FieldTree>
 
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from "vue"
-import Child from "./Child.vue"
+import { ref, onMounted } from "vue";
 
-import { apiListTemplateTypes } from "@/api/template"
+import { apiListTemplateTypes } from "@/api/template";
 import HelloWorld from "@/components/treetable/HelloWorld.vue";
 import FieldTreeTableV1 from "@/components/fields/FieldTreeTableV1.vue";
+import FieldTree from "@/components/fields/FieldTree.vue";
 
-const count = ref()
+const fields = ref<FieldInfo[]>([{
+  fieldKey: "param",
+  description: "描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息",
+  children: [{
+    fieldKey: "userName",
+    description: "userAge"
+  }]
+}]);
+
+const count = ref();
 
 onMounted(() => {
   apiListTemplateTypes().then((res) => {
-    count.value = res.data?.length
-  })
-})
+    count.value = res.data?.length;
+  });
+});
 </script>
